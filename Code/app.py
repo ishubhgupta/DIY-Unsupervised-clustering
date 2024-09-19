c66915a97c95958dbbceead9282d1bbc4ca6e968
Akshat Rastogi <akshatrastogi6704@gmail.com>
2024-09-19 09:43:02 +0530

    tabs created

--- Code/app.py
+++ Code/app.py
@@ -28,20 +28,28 @@
 import pandas as pd
 import streamlit as st # For building the web app
 
-st.set_page_config(page_title="Unsupervised Learning", page_icon=":cash:", layout="centered")
-st.markdown("<h1 style='text-align: center; color: white;'>Unsupervised Learning </h1>", unsafe_allow_html=True)
+st.set_page_config(page_title="Unsupervised Clustering", page_icon=":cash:", layout="centered")
+st.markdown("<h1 style='text-align: center; color: white;'>Unsupervised Clustering </h1>", unsafe_allow_html=True)
 st.divider()
 
-uploaded_file = st.file_uploader("Upload your dataset (CSV format)", type="csv")
-
-if uploaded_file is not None:
-    df = pd.read_csv(uploaded_file)
-
-else:
-    df = pd.read_csv("MOCK_DATA.csv")
-
-st.write("Here is a preview of your data:")
-st.write(df.head())
 
 tab1, tab2, tab3, tab4 =  st.tabs(["Model Config", "Model Training", "Model Evaluation", "Model Prediction"])
 
+with tab1:  
+    uploaded_file = st.file_uploader("Upload your dataset (CSV format)", type="csv")
+
+    if uploaded_file is not None:
+        df = pd.read_csv(uploaded_file)
+
+    else:
+        df = pd.read_csv("MOCK_DATA.csv")
+
+    st.write("Here is a preview of your data:")
+    st.write(df.head())
+
+with tab2:
+    st.subheader("Model Training")
+    st.write("This is where you can train the model.")
+    st.divider()
+    
+    st.markdown("<h3 style='text-align: center; color: white;'>K-Means</h3>", unsafe_allow_html=True)
